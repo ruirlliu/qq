76b8addd9c89c208cfd3aa1faaa8eaf0792478e6
lr <rl.liu@outlook.com>
2024-10-17 14:57:26 +0800

    Optimize the script with concurrentcy

--- scripts/import_excel_to_db.py
+++ scripts/import_excel_to_db.py
@@ -4,7 +4,18 @@ import psycopg2
 from psycopg2 import sql
 from pathlib import Path
 from psycopg2.extras import execute_values
+from concurrent.futures import ProcessPoolExecutor
 
+
+
+# Define your PostgreSQL connection parameters
+DB_PARAMS = {
+    "host":"127.0.0.1",
+    "port":"5432",
+    "database":"postgres",
+    "user":"postgres",
+    "password":"1234561",
+}
 
 def get_excel_files(directory):
     return [str(file) for file in Path(directory).rglob("*.xlsx")]
@@ -57,55 +68,46 @@ def insert_data(cur, table_name, df):
     #             VALUES (%s, %s, %s, %s, %s)",
     #             values)
 
-def load_excel_to_postgresql(directory, conn, chunk_size=1000):
-    files = get_excel_files(directory)
+def load_excel_to_postgresql(file, chunk_size=1000):
+    conn = psycopg2.connect(**DB_PARAMS)
+    cur = conn.cursor()
+    try:
+        xls = pd.ExcelFile(file)
+        for sheet_name in xls.sheet_names:
+            df = pd.read_excel(xls, sheet_name=sheet_name, usecols='A:L', engine='openpyxl')
+            print(len(df))
+            # chunk_range = range(0, len(df), chunk_size)
+
+            # for i in chunk_range:
+            #     chunk_range.count
+            #     df_chunk = df[i: i + chunk_size]
+            for df_chunk in [df[i: i + chunk_size] for i in range(0, len(df), chunk_size)]:
+                # print(len(df_chunk))
+                with conn.cursor() as cur:
+                    insert_data(cur, "citizens", df_chunk)
+                    conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print(f"Error processing {file}: {e}")
+    finally:
+        cur.close()
+        conn.close()
+
+def main():
+    # Load data from Excel files to PostgreSQL
+    directory_path = '/excel_path'
+
+    files = get_excel_files(directory_path)
 
     print(f"files = {len(files)}")
 
-    for i, file in enumerate(files):
-        print(f"i = {i}, file = {file}")
-        try:
-            xls = pd.ExcelFile(file)
-            for sheet_name in xls.sheet_names:
-                df = pd.read_excel(xls, sheet_name=sheet_name, usecols='A:L')
-                print(len(df))
-                # chunk_range = range(0, len(df), chunk_size)
-
-                # for i in chunk_range:
-                #     chunk_range.count
-                #     df_chunk = df[i: i + chunk_size]
-                for df_chunk in [df[i: i + chunk_size] for i in range(0, len(df), chunk_size)]:
-                    # print(len(df_chunk))
-                    with conn.cursor() as cur:
-                        insert_data(cur, "citizens", df_chunk)
-                        conn.commit()
-        except Exception as e:
-            print(e)
+    with ProcessPoolExecutor() as executor:
+    # Map the process_file function to the list of excel_files
+        for i, file in enumerate(files):
+            print(f"i = {i}, file = {file}")
+            executor.submit(load_excel_to_postgresql, file, chunk_size=1000)
 
 
-def chunk_insert(df, chunk_size):
-    print(len(df))
-    for df_chunk in [df[i: i + chunk_size] for i in range(0, len(df), chunk_size)]:
-        print(len(df_chunk))
-        with conn.cursor() as cur:
-            insert_data(cur, "citizens", df_chunk)
-            conn.commit()
-
-
-# Connection parameters
-conn = psycopg2.connect(
-    host="127.0.0.1",
-    port="5432",
-    database="postgres",
-    user="postgres",
-    password="1234561"
-)
-
-
-# Load data from Excel files to PostgreSQL
-directory_path = '/excel_path'
-load_excel_to_postgresql(directory_path, conn, 1000)
-
-# Close the connection
-conn.close()
+if __name__ == "__main__":
+    main()
 
